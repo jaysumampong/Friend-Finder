bf8de13f96e3648df9051d842c2de9e1208e7191
Jay Sumampong <jaysumampong@gmail.com>
2019-09-11 19:27:06 -0600

    wrote some code on apiRoutes.js

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -1,46 +1,15 @@
-// file should require the basic npm packages we've used in class: express and path.
-let require = require("http");
-let port = 8080;
-function handleRequest(req, res) {
-    response.end("Working:" + req.url);
-}
+let express = require("express");
 
-let server = http.createServer(handleRequest);
+let app = express();
 
-server.listen(port, function() {
-    console.log("server is listening: on http://localhost:" + port)
+let PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT:" + PORT);
 });
-
-let path = req.url;
-
-switch(path) {
-
-    case "/":
-        return displayRoot(res);
-
-    case "/friend-finder":
-        return displayFriendFinder(res);
-
-    default:
-        return display404(path, res);
-}
-
-function displayRoot(res) {
-    let roothmtl = "<html>" + 
-    "<body><h1>Home Page</h1>" +
-    "<a href='/friend-finder'>friend-finder</a>" +
-    "</body></html>";
-
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.end(roothmtl);
-}
-
-function display404(url,res) {
-    let roothtml = "<html>" +
-    "<body><h1>404 Not found </h1>" +
-    "<p> The page you were looking for:" + url + "can not be found</p>" +
-    "</body></html>";
-    res.writeHead(404, {"Content-Type": "text/html"});
-    res.end(roothmtl);
-}
-
