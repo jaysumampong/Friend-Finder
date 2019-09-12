861fc396a4a41eae95c42ecb090100010486baa7
Jay Sumampong <jaysumampong@gmail.com>
2019-09-11 19:48:12 -0600

    updated server.js

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -1,11 +1,16 @@
+// dependencies
 let express = require("express");
+let path = require("path");
+let bodyParser = require("body-parser");
 
 let app = express();
 
 let PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+app.use(express.static(__dirname + "/app/css"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json"}));
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
