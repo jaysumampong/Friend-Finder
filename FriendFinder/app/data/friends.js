2b04382ae5b41d8aa677354ae9ad969268592295
Jay Sumampong <jaysumampong@gmail.com>
2019-10-06 19:37:37 -0600

    added more in friends.js

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -1,8 +1,32 @@
 let friends = [
     {
-        "name":"",
+        "name":"John Snow",
         "photo":"",
-        "scores": [5,3,2,1]
+        "scores": [5,3,2,1,1,2,3,4,5,4]
+    },
+
+    {
+        "name":"John Wick",
+        "photo":"",
+        "scores": [4,1,2,3,5,1,2,4,3,2]
+    },
+
+    {
+        "name":"John Cena",
+        "photo":"",
+        "scores": [1,2,1,3,4,1,5,5,5,2]
+    },
+
+    {
+        "name":"John Legend",
+        "photo":"",
+        "scores": [5,3,2,1,2,2,3,3,1,4]
+    },
+
+    {
+        "name":"John Mayer",
+        "photo":"https://www.imdb.com/name/nm1243604/mediaviewer/rm1097767680",
+        "scores": [5,3,2,1,4,2,1,4,3,5]
     }
 ]
 
