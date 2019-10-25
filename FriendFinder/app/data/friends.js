7db50abfead1fa827b5924fab1b13a9af5d55b8f
Jay Sumampong <jaysumampong@gmail.com>
2019-10-25 14:48:59 -0600

    tried some changes to figure out why modal isnt working

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -1,31 +1,31 @@
 let friends = [
     {
         name:"John Snow",
-        photo:"http://selfninja.com/wp-content/uploads/2016/12/Cover-2.jpg",
+        // photo:"http://selfninja.com/wp-content/uploads/2016/12/Cover-2.jpg",
         scores: [5,3,2,1,1,2,3,4,5,4]
     },
 
     {
         name:"John Wick",
-        photo:"https://i.ytimg.com/vi/0ESDnRQIX7A/maxresdefault.jpg",
+        // photo:"https://i.ytimg.com/vi/0ESDnRQIX7A/maxresdefault.jpg",
         scores: [4,1,2,3,5,1,2,4,3,2]
     },
 
     {
         name:"John Cena",
-        photo:"https://statics.sportskeeda.com/wp-content/uploads/2014/05/capture-1398998730.jpg",
+        // photo:"https://statics.sportskeeda.com/wp-content/uploads/2014/05/capture-1398998730.jpg",
         scores: [1,2,1,3,4,1,5,5,5,2]
     },
 
     {
         name:"John Stewart",
-        photo:"https://media.npr.org/assets/img/2015/08/05/ap_681282264176_wide-6831f5aad5cfcfa8945267d357bba3dbebb7ccf7-s800-c85.jpg",
+        // photo:"https://media.npr.org/assets/img/2015/08/05/ap_681282264176_wide-6831f5aad5cfcfa8945267d357bba3dbebb7ccf7-s800-c85.jpg",
         scores: [5,3,2,1,2,2,3,3,1,4]
     },
 
     {
         name:"John Mayer",
-        photo:"https://media.newyorker.com/photos/59097fa9c14b3c606c109c9e/master/w_727,c_limit/Petrusich-John-Mayer.jpg",
+        // photo:"https://media.newyorker.com/photos/59097fa9c14b3c606c109c9e/master/w_727,c_limit/Petrusich-John-Mayer.jpg",
         scores: [5,3,2,1,4,2,1,4,3,5]
     }
 ]
